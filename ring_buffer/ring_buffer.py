d71f4d326a45d30e7e0b9b358e16cf0149405c2d
Justin Trombley <jtrombley96@gmail.com>
2020-05-14 22:42:15 -0400

    Reverse MVP + Stretch

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = 5
+        self.data = ['a', 'b', 'c', 'd', 'e']
+
 
     def append(self, item):
-        pass
+        # Should add the given element to the buffer.
+        self.data.pop(0)
+        self.data.append(item)
 
     def get(self):
-        pass
+        # Returns all of the elements in the buffer 
+        # in a list in their given order
+        return self.data
